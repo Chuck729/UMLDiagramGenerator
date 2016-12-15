1fc5621475451d2f3d0996836bb8da8b1a0bfedf
hauges <hauges@rose-hulman.edu>
2016-12-15 17:49:29 -0500

    small additions to DesignParser

--- code/src/main/java/edu/rosehulman/csse374/revengd/DesignParser.java
+++ code/src/main/java/edu/rosehulman/csse374/revengd/DesignParser.java
@@ -2,17 +2,22 @@ package edu.rosehulman.csse374.revengd;
 
 import java.util.List;
 
+import org.objectweb.asm.ClassReader;
+
 public class DesignParser implements IDesignParser {
 	
 	private List<IClassContent> classes;
 	private ICodeGenerator codeGenerator;
 	private String outFile;
+	private ClassReader reader;
+	private List<String> classNames;
 
 	// TODO add parameter for recursiveParsing and accessLevel
 	
-	public DesignParser(ICodeGenerator codeGenerator, String outFile) {
+	public DesignParser(ICodeGenerator codeGenerator, String outFile, List<String> classNames) {
 		this.codeGenerator = codeGenerator;
 		this.outFile = outFile;
+		this.classNames = classNames;
 	}
 
 	@Override
