9e69c4bd0a0de40803a6bb57635cd3e1975e4de1
hauges <hauges@rose-hulman.edu>
2017-01-25 18:28:25 -0500

    untested inheritence over composition

--- code/src/main/java/design/parsers/CompOverInherParser.java
+++ code/src/main/java/design/parsers/CompOverInherParser.java
@@ -1,10 +1,18 @@
 package design.parsers;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.ClassNode;
+
+import edu.rosehulman.csse374.revengd.ClassContent;
 import edu.rosehulman.csse374.revengd.IClassContent;
 
 public class CompOverInherParser extends PatternDecorator {
+	
+	private final String OPTION = "color";
+	private final String VALUE = "orange";
 	
 	public CompOverInherParser(IDesignParser d) {
 		this.decorated = d;
@@ -17,8 +25,14 @@ public class CompOverInherParser extends PatternDecorator {
 		List<IClassContent> classContent = this.decorated.getClassContent();
 		
 		for (IClassContent clazz : classContent) {
-			if (clazz.getParent().equals("")) {
-				
+			if (clazz.getParent() == null || clazz.getParent().equals("java.lang.Object")) {
+				// do nothing
+			} else {
+				String parent = clazz.getParent();
+				IClassContent parentContent = new ClassContent(parent);
+				if (!parentContent.isAbstract()) {
+					clazz.addOption(OPTION, VALUE);
+				}
 			}
 		}
 	}
